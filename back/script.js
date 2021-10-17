a7c85275518e102e65fc25bab377ac1caaafd07b
Faradji Boucif <Dw.faradji66@yahoo.com>
2021-10-17 11:38:47 +0200

    Ajout des balise Html et des information produit Récuperer dans l'Api et construction de la boucle

--- back/script.js
+++ back/script.js
@@ -7,6 +7,41 @@ async function chargementDonnee() {
     //reponse de l'Api
     let arrayProduct = await response.json();
     console.log(arrayProduct);
+
+    //selection de l'Id pour integrer le bloc article et ses enfants
+    function addBloc_article(produit) {
+      const selectId = document.getElementById("items");
+
+      const addHref = document.createElement("a");
+      addHref.href = "product.html?id=" + produit._id;
+
+      selectId.appendChild(addHref);
+
+      const addBalise = document.createElement("article");
+      addHref.appendChild(addBalise);
+
+      //-------------image----------
+      const addImg = document.createElement("img");
+      addImg.src = produit.imageUrl;
+      addImg.alt = produit.altTxt;
+      addBalise.appendChild(addImg);
+
+      //------Balise-------
+      const addHn = document.createElement("h3");
+      addBalise.appendChild(addHn);
+      addHn.innerHTML = produit.name;
+      addHn.classList.add("productName");
+
+      const addP = document.createElement("p");
+      addBalise.appendChild(addP);
+      addP.innerHTML = produit.description;
+      addP.classList.add("productDescription");
+    }
+
+    // Boucle Ajout des bloc article
+    arrayProduct.forEach((product) => {
+      addBloc_article(product);
+    });
   } catch (e) {}
 }
 chargementDonnee();
